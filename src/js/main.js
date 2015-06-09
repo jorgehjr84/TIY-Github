f0eeba9f1c712d6a062cf1422bd02f5618cff455
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-09 16:29:02 -0400

    Placeholders in for API data to come later

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+(function(window) {
+
+  $(document).ready(function() {
+
+
+
+    //Where is the URL for the avatar
+
+
+
+    // });
+    // //
+    // //   //Where is the URL for the avatar
+    // //   console.log(octocat.avatar_url);
+
+
+    // //   //Where does the URL go in my HTML page?
+    // //   $avatar.attr('src', octocat.avatar_url);
+    // //
+    // //   //What do we do once we have the underpants?
+    // //   $avatar.attr('src', octocat.avatar_url);
+    // // });
+
+
+
+
+
+  }); //End of document ready function
+
+})(window);
