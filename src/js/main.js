3e8f2faadb049e3ac39e4cc8851072cceb30a97f
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-09 21:07:30 -0400

    Still trying to get rid of my 404 error

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,23 @@
   $(document).ready(function() {
 
 
-
-    //Where is the URL for the avatar
-
-
-
+    $.getJSON('../api/github/users/octocat.json').then(function(octocat) {
+      console.log(octocat.avatar_url);
+    });
+    // //Where is the URL for the avatar
+    // $.getJSON('../api/github/users/octocat/octocat.json').then(function(octocat) {
+    //
+    //   //Where is the URL for the avatar
+    //   console.log(octocat.avatar_url);
+    //
+    //
+    //   //Where does the URL go in my HTML page?
+    //   $avatar.attr('src', octocat.avatar_url);
+    //
+    //   //What do we do once we have the underpants?
+    //   $avatar.attr('src', octocat.avatar_url);
+    //
     // });
-    // //
-    // //   //Where is the URL for the avatar
-    // //   console.log(octocat.avatar_url);
-
-
-    // //   //Where does the URL go in my HTML page?
-    // //   $avatar.attr('src', octocat.avatar_url);
-    // //
-    // //   //What do we do once we have the underpants?
-    // //   $avatar.attr('src', octocat.avatar_url);
-    // // });
-
-
-
 
 
   }); //End of document ready function
