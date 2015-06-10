f39ae11f082837182e5a93f674fd57523682ff5b
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-10 16:01:15 -0400

    Columns on top of page for reference. Going to work on resizing now

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,44 @@
 (function(window) {
 
-  $(document).ready(function() {
+  $.getJSON('../api/github/users/octocat.json').then(function(octocat) {
+    // Where is the URL for the avatar? octocat.avatar_url
+    // console.log(octocat);
+    // Where does the URL go in my HTML page?
+    $avatar = $('#octocat-img');
+    $avatar.attr('src', octocat.avatar_url);
+    // //   // What do we do once we have the underpants?
+
+    // $('[data-bind="user.login"]').text(octocat.login)
 
 
+    $name = $('#name');
+    $name.text(octocat.name);
 
-  }); //End of document ready function
+    $login = $('#login');
+    $login.text(octocat.login);
+
+    $organizations = $('.organizations');
+    $organizations.text(octocat.company);
+
+    $location = $('.location');
+    $location.text(octocat.location);
+
+    $email = $('.email');
+    $email.text(octocat.email);
+
+    $blog = $('.blog ');
+    $blog.text(octocat.blog);
+
+    $followers = $('.followers');
+    $followers.text(octocat.followers);
+
+    $starred = $('.starred');
+    $starred.text(octocat.starred);
+
+    $following = $('.following');
+    $following.text(octocat.following);
+
+
+  });
 
 })(window);
